4ab2f904368fb54cc4c15f5546562e932f1a28e1
Bernhard Danecker <bernhard.danecker@gmail.com>
2018-10-03 15:03:16 +0200

    Request7Future

--- src/project/Request7Future.kt
+++ src/project/Request7Future.kt
@@ -4,4 +4,6 @@ import kotlinx.coroutines.*
 import kotlinx.coroutines.future.*
 import java.util.concurrent.*
 
-fun loadContributorsConcurrentAsync(req: RequestData): CompletableFuture<List<User>> = TODO()
+fun loadContributorsConcurrentAsync(req: RequestData): CompletableFuture<List<User>> = GlobalScope.future {
+    loadContributorsConcurrent(req)
+}
