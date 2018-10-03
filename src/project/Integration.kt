24fa4f1d1cd71d190e0e1c41e8c67b8042c57d88
Bernhard Danecker <bernhard.danecker@gmail.com>
2018-10-03 11:51:57 +0200

    suspendCancellableCoroutine

--- src/project/Integration.kt
+++ src/project/Integration.kt
@@ -4,7 +4,7 @@ import kotlinx.coroutines.*
 import retrofit2.*
 import kotlin.coroutines.*
 
-suspend fun <T> Call<T>.await(): T = suspendCoroutine { cont ->
+suspend fun <T> Call<T>.await(): T = suspendCancellableCoroutine { cont ->
     enqueue(object : Callback<T> {
         override fun onResponse(call: Call<T>, response: Response<T>) {
             if (response.isSuccessful) {
@@ -18,8 +18,11 @@ suspend fun <T> Call<T>.await(): T = suspendCoroutine { cont ->
             cont.resumeWithException(t)
         }
     })
+    cont.invokeOnCancellation {
+        cancel()
+    }
 }
 
 class ErrorResponse(response: Response<*>) : Exception(
-    "Failed with ${response.code()}: ${response.message()}\n${response.errorBody()?.string()}"
+        "Failed with ${response.code()}: ${response.message()}\n${response.errorBody()?.string()}"
 )
