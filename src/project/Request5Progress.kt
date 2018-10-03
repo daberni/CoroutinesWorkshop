9c2b9ec32286b0c8ab37ddf4740c7830894e0281
Bernhard Danecker <bernhard.danecker@gmail.com>
2018-10-03 11:02:30 +0200

    Request5Progress

--- src/project/Request5Progress.kt
+++ src/project/Request5Progress.kt
@@ -1,6 +1,21 @@
 package project
 
 suspend fun loadContributorsProgress(req: RequestData, callback: (List<User>) -> Unit) {
-    TODO()
-}
+    val service = createGitHubService(req.username, req.password)
+    log.info("Loading ${req.org} repos")
 
+    val repos = service.listOrgRepos(req.org).await()
+    log.info("${req.org}: loaded ${repos.size} repos")
+
+    var contribs = listOf<User>()
+    for (repo in repos) {
+        val users = service.listRepoContributors(req.org, repo.name).await()
+        log.info("${repo.name}: loaded ${users.size} contributors")
+
+        contribs = (contribs + users).aggregate()
+        callback(contribs)
+    }
+
+    log.info("Total: ${contribs.size} contributors")
+    callback(contribs)
+}
